fdf933e169caccb91a03646b1e0cc0c25ee3a427
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-04 14:59:49 -0400

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,73 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES
+("Amazing Andy", "Tech", 10000, 2016-06-13, 2016-07-13),
+("Jumpin' Jasper", "Art", 30000, 2016-07-20, 2016-09-13),
+("The Human Fund", "Charity", 1000000, 2016-06-01, 2017-06-01),
+("Tree Man", "Indie Film", 50000, 2015-10-10, 2016-12-31),
+("Mega Mug", "Gadget", 20000, 2016-07-10, 2017-01-13),
+("Inside Out Pen", "Gadget", 40000, 2016-10-01, 2016-11-01),
+("Sexy Reading Glasses", "Fashion", 5000, 2016-08-01, 2016-08-20),
+("Holy Guacamole", "Religion", 5000, 2016-12-01, 2016-12-31),
+("Bloodsuckers: The Book", "Literature", 15000, 2017-01-01, 2017-07-13),
+("Levitating Coaster", "Tech", 80000, 2016-06-01, 2016-08-15);
+
+INSERT INTO users (name, age)
+VALUES
+("Sarah", 23),
+("Sara", 16),
+("S-rah", 24),
+("Sar-bear", 43),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23),
+("Sarah", 23);
+
+
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3),
+(50, 1, 3);
+
+
+
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,53 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+"SELECT title, SUM(pledges.amount)
+FROM projects
+JOIN pledges
+ON pledges.project_id = projects.id
+GROUP BY pledges.project_id
+ORDER BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+"SELECT name, age, SUM(pledges.amount)
+FROM users
+JOIN pledges
+ON pledges.user_id = users.id
+GROUP BY users.id
+ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT title, SUM(pledges.amount) - funding_goal
+FROM projects
+JOIN pledges
+ON pledges.project_id = projects.id
+GROUP BY pledges.project_id
+HAVING SUM(pledges.amount) - funding_goal >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+"SELECT name, SUM(pledges.amount)
+FROM users
+JOIN pledges
+ON pledges.user_id = users.id
+GROUP BY users.name
+ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT category, pledges.amount
+FROM projects
+JOIN pledges
+ON pledges.project_id = projects.id
+WHERE category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+"SELECT category, SUM(pledges.amount)
+FROM projects
+JOIN pledges
+ON pledges.project_id = projects.id
+WHERE category = 'books'"
 end
